224c87b27fca55352033475b036a380d36b16d35
hatim <hatimacharaoui@gmail.com>
2025-11-07 16:20:27 +0100

    purchase-logic

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -14,6 +14,85 @@ public class VendingMachine {
 
     // afficherProduits
     // acheterProduit
+
+    Scanner scanner = new Scanner(System.in);
+    public static int acheterProduit(ArrayList<Integer> stock){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entrez le numéro du produit : ");
+        int choix = scanner.nextInt();
+        int montant = 0;
+
+        switch (choix) {
+            case 1:
+                do {
+                    System.out.println("Entrez le montant inséré : ");
+                    montant = scanner.nextInt();
+                    if (montant < 5) {
+                        System.out.println("Monnaie insuffisante");
+                    }
+                }while (montant < 5);
+
+                if (stock.get(0) > 1) {
+                    System.out.println("Vous avez acheté : Eau");
+                    int rendue = montant - 5;
+                    System.out.println("Monnaie rendue : " + rendue + " MAD");
+
+                } else {
+                    System.out.println("stock insuffisant");
+                }
+                break;
+            case 2:
+                do {
+                    System.out.println("Entrez le montant inséré : ");
+                    montant = scanner.nextInt();
+                    if (montant < 8) {
+                        System.out.println("Monnaie insuffisante");
+                    }
+                }while (montant < 8);
+
+                if (stock.get(1) > 1) {
+                    System.out.println("Vous avez acheté : Soda");
+                    int rendue = montant - 8;
+                    System.out.println("Monnaie rendue : "+ rendue +" MAD");
+
+                } else { System.out.println("stock insuffisant");}
+
+                break;
+            case 3:
+                do {
+                    System.out.println("Entrez le montant inséré : ");
+                    montant = scanner.nextInt();
+                    if (montant < 12) {
+                        System.out.println("Monnaie insuffisante");
+                    }
+                }while (montant < 12);
+
+                if (stock.get(1) > 1) {
+                    System.out.println("Vous avez acheté : Chips");
+                    int rendue = montant - 12;
+                    System.out.println("Monnaie rendue : "+ rendue +" MAD");
+
+                } else { System.out.println("stock insuffisant");}
+                break;
+            case 4:
+                do {
+                    System.out.println("Entrez le montant inséré : ");
+                    montant = scanner.nextInt();
+                    if (montant < 15) {
+                        System.out.println("Monnaie insuffisante");
+                    }
+                }while (montant < 15);
+                if (stock.get(1) > 1) {
+                    System.out.println("Vous avez acheté : Chocolat");
+                    int rendue = montant - 15;
+                    System.out.println("Monnaie rendue : "+ rendue +" MAD");
+
+                } else { System.out.println("stock insuffisant");}
+                break;
+        }
+        return choix;
+    }
+
     // afficherStock
     public static void main(String[] args) {
         ArrayList<String> produits = new ArrayList<>(List.of("Eau", "Soda", "Chips", "Chocolat"));
