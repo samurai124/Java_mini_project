a53c0e657397042c25a506d2e1b2677153f74f41
badrbelabrik <badr.belabrik@gmail.com>
2025-11-07 15:38:22 +0100

    affiche du produits

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -3,13 +3,20 @@ public class VendingMachine {
     // Distributeur Automatique
     // menu-display
     // afficherProduits
+    public static void afficherProduits(List<String> produits, ArrayList<Double> prix, List<Integer> stock) {
+        System.out.println("Produits disponibles :");
+        for (int i=0; i< produits.size(); i++){
+            System.out.println((i+1)+". "+ produits.get(i) +" ( "+prix.get(i)+ " MAD) - Stock : "+stock.get(i));
+        }
+    }
+
     // acheterProduit
     // afficherStock
     public static void main(String[] args) {
         ArrayList<String> produits = new ArrayList<>(List.of("Eau", "Soda", "Chips", "Chocolat"));
         ArrayList<Double> prix = new ArrayList<>(List.of(5.0, 8.0, 12.0, 15.0));
         ArrayList<Integer> stock = new ArrayList<>(List.of(10, 5, 7, 3));
-
+        afficherProduits(produits,prix,stock);
 
     }
 }
