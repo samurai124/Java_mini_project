24fbe780d78b9ecd64d2c38aca81397615822afa
ennafiaasma-art <ennafiaasma@gmail.com>
2025-11-07 15:01:21 +0100

    menu principal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     // Distributeur Automatique
     // menu-display
+    public static int menuprincipal() {
+        Scanner scanner = new Scanner(System.in);
+        int choix =0;
+        System.out.println("bienvenus ");
+        System.out.println("1. Afficher le produit\n 2.Achater\n 3.quitter\n");
+        System.out.println("entrer votre choix :  ");
+        choix=scanner.nextInt();
+        return choix;
+    }
+
     // afficherProduits
     // acheterProduit
     // afficherStock
@@ -9,7 +21,11 @@ public class Main {
         ArrayList<String> produits = new ArrayList<>(List.of("Eau", "Soda", "Chips", "Chocolat"));
         ArrayList<Double> prix = new ArrayList<>(List.of(5.0, 8.0, 12.0, 15.0));
         ArrayList<Integer> stock = new ArrayList<>(List.of(10, 5, 7, 3));
-
+        System.out.println(menuprincipal());
 
     }
+
 }
+
+
+
