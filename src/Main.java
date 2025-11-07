7302ed9de4de3a73110cb72122a8ab2e6f6007c3
Hamza <hamzazaidi253@gamil.com>
2025-11-07 11:28:29 +0100

    dividing the work

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.util.*;
 public class Main {
     // Distributeur Automatique
+    // menu-display
+    //afficherProduits
+    //acheterProduit
+    // afficherStock
     public static void main(String[] args) {
 
     }
